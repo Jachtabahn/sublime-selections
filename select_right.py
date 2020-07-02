46c1d893b52ad9b1c5ff4dbc02f1c0f159dd74c8
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2020-07-02 15:05:09 +0200

    New file for the select_left command

---
+++ select_right.py
@@ -0,0 +1,32 @@
+import sublime
+import sublime_plugin
+
+class SelectionStringInputHandler(sublime_plugin.TextInputHandler):
+
+  def placeholder(self):
+    return "Selection character"
+
+class SelectionRegexCommand(sublime_plugin.TextCommand):
+
+  def input(self, args):
+    return SelectionStringInputHandler()
+
+  def run(self, edit, selection_string):
+
+    # Get the manager of all selections in the current view
+    selection = self.view.sel()
+
+    for region in selection:
+
+      # Compute the new left border of the current cursor's selection
+      new_left_point = region.a + 1
+      current_string = None
+      while current_string not in [selection_string, '']:
+        new_left_point -= 1
+        current_region = sublime.Region(new_left_point - len(selection_string),
+          new_left_point)
+        current_string = self.view.substr(current_region)
+
+      # Extend the current cursor's selection
+      new_region = sublime.Region(new_left_point, region.b)
+      selection.add(new_region)
