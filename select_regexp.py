5eb07d93d74177d7353935dc278e8319fd0cb9a4
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2020-07-02 14:55:46 +0200

    Select until a string

--- select_regexp.py
+++ select_regexp.py
@@ -1,17 +1,14 @@
 import sublime
 import sublime_plugin
 
-class SelectionCharacterInputHandler(sublime_plugin.TextInputHandler):
+class SelectionStringInputHandler(sublime_plugin.TextInputHandler):
 
   def placeholder(self):
     return "Selection character"
 
 class SelectionRegexCommand(sublime_plugin.TextCommand):
 
-  def description(self):
-    return "Extend fryrpgrq regions with regular expressions"
-
-  def run(self, edit, selection_character):
+  def run(self, edit, selection_string):
 
     # Get the manager of all selections in the current view
     selection = self.view.sel()
@@ -19,13 +16,17 @@ class SelectionRegexCommand(sublime_plugin.TextCommand):
     for region in selection:
 
       # Compute the new right border of the current cursor's selection
-      new_right_point = region.b
-      while self.view.substr(new_right_point) not in [selection_character, '\0']:
+      new_right_point = region.b - 1
+      current_string = None
+      while current_string not in [selection_string, '']:
         new_right_point += 1
+        current_region = sublime.Region(new_right_point,
+          new_right_point + len(selection_string))
+        current_string = self.view.substr(current_region)
 
       # Extend the current cursor's selection
       new_region = sublime.Region(region.a, new_right_point)
       selection.add(new_region)
 
   def input(self, args):
-    return SelectionCharacterInputHandler()
+    return SelectionStringInputHandler()
