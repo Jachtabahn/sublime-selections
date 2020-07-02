fc123f7efc7d00f2c3955228a781469362e3c375
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2020-07-02 14:09:23 +0200

    Add InputHandler

--- select_regexp.py
+++ select_regexp.py
@@ -1,19 +1,55 @@
 import sublime
 import sublime_plugin
 
-class ExampleCommand(sublime_plugin.TextCommand):
+class SelectionCharacterInputHandler(sublime_plugin.TextInputHandler):
 
-  def run(self, edit):
+  def __init__(self, view):
+    self.view = view
+
+  def placeholder(self):
+    return "Expression"
+
+  def initial_text(self):
+    return "i+1"
+
+  def preview(self, expr):
+    try:
+      v = self.view
+      s = v.sel()
+      count = len(s)
+      if count > 5:
+        count = 5
+    except Exception:
+      return ""
+
+  def validate(self, expr):
+    try:
+      v = self.view
+      s = v.sel()
+      return True
+    except Exception:
+      return False
+
+class SelectionRegexCommand(sublime_plugin.TextCommand):
+
+  def description(self):
+    return "Extend selected regions with regular expressions"
+
+  def run(self, edit, character):
+
+    print("character")
+    print(character)
 
     # Get the manager of all selections in the current view
     selection = self.view.sel()
+    # Get the currently selected regions
     print(selection)
 
-    # Get the currently selected regions
+    left_positions = []
     right_positions = []
     for region in selection:
-      right_position = region.b
-      right_positions.append(right_position)
+      left_positions.append(region.a)
+      right_positions.append(region.b)
       print(region)
 
     desired_character = '>'
@@ -30,7 +66,10 @@ class ExampleCommand(sublime_plugin.TextCommand):
     print(new_right_positions)
 
     # Add new regions covering the extensions
-    for position in zip(right_positions, new_right_positions):
+    for position in zip(left_positions, new_right_positions):
       print(position)
       new_region = sublime.Region(position[0], position[1])
       selection.add(new_region)
+
+  def input(self, args):
+    return SelectionCharacterInputHandler(self.view)
